df0b522f17c97f9ece1037d19faabf5c72d368be
RutamBhagat <rutambhagat@gmail.com>
2024-07-04 08:28:32 +0530

    fix imports

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -4,14 +4,15 @@ pragma solidity 0.8.18;
 import { PriceConverter } from "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
+
     uint256 minimumUSD = 5e18;
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
     function fund() public payable {
-        
         // 1e18 = 1 ETH = 1000000000000000000 wei = 1 * 10^18 wei
-        require(PriceConverter.getConversionRate(msg.value) >= minimumUSD, "Funding amount must be at least 5 USD worth of ETH"); 
+        require(msg.value.getConversionRate() >= minimumUSD, "Funding amount must be at least 5 USD worth of ETH"); 
         // if there is not enough ETH, then the transaction will revert
         
         funders.push(msg.sender);
